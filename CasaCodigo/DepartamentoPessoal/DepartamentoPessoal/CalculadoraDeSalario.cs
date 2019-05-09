2b8f28b5d688203a9f8db397f61f39736ab162eb
Edilon Andrade <edilonandrade@gmail.com>
2019-05-09 01:49:25 -0300

    Primeiras refatorações no classe de cálculo do salário.

--- CasaCodigo/DepartamentoPessoal/DepartamentoPessoal/CalculadoraDeSalario.cs
+++ CasaCodigo/DepartamentoPessoal/DepartamentoPessoal/CalculadoraDeSalario.cs
@@ -8,20 +8,30 @@ namespace DepartamentoPessoal
         {
             if (funcionario.Cargo.Equals(Tipos.Cargo.DESENVOLVEDOR))
             {
-                if (funcionario.Salario > 3000) return funcionario.Salario * 0.8;
-                return funcionario.Salario * 0.9;
+                return DezOuVintePorCentoDeDesconto(funcionario);                
             }
             else if (funcionario.Cargo.Equals(Tipos.Cargo.DBA) || funcionario.Cargo.Equals(Tipos.Cargo.TESTADOR))
             {
-                if (funcionario.Salario < 2500)
-                {
-                    return funcionario.Salario * 0.85;
-                }
-                return funcionario.Salario * 0.75;
+                return QuinzeOuVinteCincoPorCentoDeDesconto(funcionario);
             }
 
             throw new Exception("Funcionário inválido.");
            
+        }
+
+        private double QuinzeOuVinteCincoPorCentoDeDesconto(Funcionario funcionario)
+        {
+            if (funcionario.Salario < 2500)
+            {
+                return funcionario.Salario * 0.85;
+            }
+            return funcionario.Salario * 0.75;
+        }
+
+        private double DezOuVintePorCentoDeDesconto(Funcionario funcionario)
+        {
+            if (funcionario.Salario > 3000) return funcionario.Salario * 0.8;
+            return funcionario.Salario * 0.9;
         }
     }
 }
