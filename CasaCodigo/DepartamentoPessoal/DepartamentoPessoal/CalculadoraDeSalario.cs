dabfa6ae1192ba723ce344fa3ca241b5eab68c24
Edilon Andrade <edilonandrade@gmail.com>
2019-05-09 14:00:16 -0300

    Refatorando com o template method.

--- CasaCodigo/DepartamentoPessoal/DepartamentoPessoal/CalculadoraDeSalario.cs
+++ CasaCodigo/DepartamentoPessoal/DepartamentoPessoal/CalculadoraDeSalario.cs
@@ -35,4 +35,22 @@ namespace DepartamentoPessoal
            
         }        
     }
+
+    public abstract class RegraCalculo
+    {
+        public double Calcula(Funcionario funcionario)
+        {
+            if (funcionario.Salario > Limite())
+            {
+                return funcionario.Salario * PorcentagemAcimaDoLimite();
+            }
+            return funcionario.Salario * PorcentagemBase();
+        }
+
+        protected abstract int Limite();
+
+        protected abstract double PorcentagemAcimaDoLimite();
+
+        protected abstract double PorcentagemBase();
+    }
 }
