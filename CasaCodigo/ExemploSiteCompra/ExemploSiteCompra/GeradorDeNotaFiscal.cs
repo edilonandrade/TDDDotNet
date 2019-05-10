9890d7584b11bb5e657da09099fb60387c88ec08
Edilon Andrade <edilonandrade@gmail.com>
2019-05-10 10:10:37 -0300

    Inclusão do exemplo de teste de método estático.

--- CasaCodigo/ExemploSiteCompra/ExemploSiteCompra/GeradorDeNotaFiscal.cs
+++ CasaCodigo/ExemploSiteCompra/ExemploSiteCompra/GeradorDeNotaFiscal.cs
@@ -9,15 +9,20 @@ namespace ExemploSiteCompra
     public class GeradorDeNotaFiscal
     {
         private IList<IAcaoAposGerarNota> acoes;
+        private IRelogio relogio;
 
-        public GeradorDeNotaFiscal(IList<IAcaoAposGerarNota> acoes)
+        public GeradorDeNotaFiscal(IList<IAcaoAposGerarNota> acoes, IRelogio relogio)
         {
             this.acoes = acoes;
+            this.relogio = relogio;
         }
+
+        //construtor sem relógio para não quebrar o resto do sistema
+        public GeradorDeNotaFiscal(IList<IAcaoAposGerarNota> acoes) : this(acoes, new RelogioDoSistema()) { }
 
         public NotaFiscal Gera(Pedido pedido)
         {
-            NotaFiscal nf = new NotaFiscal(pedido.Cliente, pedido.ValorTotal * 0.94, DateTime.Now);
+            NotaFiscal nf = new NotaFiscal(pedido.Cliente, pedido.ValorTotal * 0.94, relogio.Hoje());
 
             foreach(var acao in acoes)
             {
---
+++ CasaCodigo/ExemploSiteCompra/ExemploSiteCompra/RelogioDoSistema.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExemploSiteCompra
+{
+    public interface IRelogio
+    {
+        DateTime Hoje();
+    }
+
+    public class RelogioDoSistema : IRelogio
+    {
+        public DateTime Hoje()
+        {
+            return DateTime.Now;
+        }
+    }
+}
